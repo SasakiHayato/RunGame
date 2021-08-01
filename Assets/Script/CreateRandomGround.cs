7701ad2f65a9021faccde1da0ec998724f7a0491
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-08-02 00:38:39 +0900

    Groundのランダム生成を追加

--- Assets/Script/CreateRandomGround.cs
+++ Assets/Script/CreateRandomGround.cs
@@ -7,8 +7,33 @@ public class CreateRandomGround : MonoBehaviour
     [SerializeField] private Transform[] m_pos;
     [SerializeField] private GameObject[] m_ground;
 
-    void Start()
+    private float m_timer = 0;
+
+    void Update()
     {
-        
+        m_timer += Time.deltaTime;
+
+        if (m_timer > 1.5f)
+        {
+            Transform pos = SelectPos();
+            GameObject ground = SelectGround();
+
+            Instantiate(ground, pos);
+
+            m_timer = 0;
+        }
+    }
+
+    private Transform SelectPos()
+    {
+        int random = Random.Range(0, m_pos.Length);
+
+        return m_pos[random];
+    }
+
+    private GameObject SelectGround()
+    {
+        int random = Random.Range(0, m_ground.Length);
+        return m_ground[random];
     }
 }

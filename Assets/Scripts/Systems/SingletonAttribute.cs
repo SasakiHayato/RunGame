32eee9e0411745301dd8f399a594315c8d2a22e9
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-02-26 14:50:14 +0900

    Singletonの生成クラスの作成

---
+++ Assets/Scripts/Systems/SingletonAttribute.cs
@@ -0,0 +1,38 @@
+﻿
+/// <summary>
+/// Singletonにするクラスに継承するクラス
+/// </summary>
+/// <typeparam name="Sigleton">派生クラス</typeparam>
+
+public class SingletonAttribute<Sigleton> where Sigleton : class
+{
+    bool _isSetUp = false;
+
+    Sigleton _sigleton;
+    public static Sigleton Access
+    {
+        get
+        {
+            if (Instance._isSetUp) return Instance._sigleton;
+            else return null;
+        }
+    }
+
+    private static SingletonAttribute<Sigleton> _instance = null;
+    public static SingletonAttribute<Sigleton> Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new SingletonAttribute<Sigleton>();
+                _instance.SetUp();
+            }
+
+            return _instance;
+        }
+    }
+
+    public virtual void SetUp() => _isSetUp = true;
+    public void Destory() => _instance.Destory();
+}
